11a3a31c95bf08cf059dd83f6ba48af159bb38f2
adalomer <adalomer60@gmail.com>
2026-01-02 13:27:24 +0000

    update v5.1

--- momez/app/(public)/urun/[slug]/page.tsx
+++ momez/app/(public)/urun/[slug]/page.tsx
@@ -386,11 +386,6 @@ export default function ProductDetailPage({ params }: { params: Promise<{ slug: 
                   )
                 })}
               </div>
-              {selectedSize && availableStock > 0 && (
-                <p className="text-xs text-green-600 dark:text-green-400 mt-2">
-                  ✓ {availableStock} {t('product.stockCount')} {t('product.inStock')}
-                </p>
-              )}
             </div>
 
             {/* Miktar */}
